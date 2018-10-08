42289718911889ac6c4f967e51e81bfb2678c426
notyourbear <pracenberg@gmail.com>
2018-10-08 15:55:43 -0700

    popups work

--- docs/script.js
+++ docs/script.js
@@ -5,18 +5,20 @@ firebase.database().ref('/').once('value').then(function(snapshot) {
   var data = snapshot.val()
 
   Object.values(data).forEach(function(datum) {
-    var li = document.createElement('div');
+    var li = document.createElement('li');
     li.classList.add('masonry-layout__panel');
 
     var div = document.createElement('div');
     div.classList.add('masonry-layout__panel-content');
     div.classList.add('margin');
     var mySVG64 = window.btoa(datum.inline);
-    var innerHTML = `<img class="lazyload pointer"
+    var innerHTML = `
+        <img class="lazyload pointer"
           alt="${datum.title}"
           src="data:image/svg+xml;base64,${mySVG64}"
           data-src="${datum.imgSrc}">
         <div class="hidden description">
+          <button class='xout'>x</button>
           <h3>${datum.title}</h3>
           <div>${datum.desc}</div>`;
 
@@ -25,8 +27,8 @@ firebase.database().ref('/').once('value').then(function(snapshot) {
     innerHTML += `</div>`;
 
     div.innerHTML = innerHTML;
+    div.addEventListener('click', openPopup)
     li.appendChild(div);
-    div.addEventListener('click', onClick)
     fragment.appendChild(li);
   })
 
@@ -34,8 +36,28 @@ firebase.database().ref('/').once('value').then(function(snapshot) {
   lazyload();
 });
 
-function onClick(e) {
+function openPopup(e) {
   e.preventDefault();
+  console.log('open', this)
+  this.removeEventListener('click', openPopup);
+
   var descriptorBlock = this.querySelector('.description');
   descriptorBlock.classList.toggle('hidden');
+  var xout = this.querySelector('button');
+  xout.addEventListener('click', closePopup);
+}
+
+function closePopup(e) {
+  e.preventDefault();
+  this.removeEventListener('click', closePopup);
+  this.parentElement.classList.toggle('hidden');
+
+  var parent =  this.parentElement;
+  while (!parent.classList.contains('masonry-layout__panel-content')) {
+    parent = parent.parentElement;
+  }
+  
+  setTimeout(() => {
+    parent.addEventListener('click', openPopup);
+  });
 }
