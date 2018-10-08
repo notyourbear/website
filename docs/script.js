0be2d6e78d3c849a3af99c44a2d7e7d062238bdc
notyourbear <pracenberg@gmail.com>
2018-10-08 14:09:19 -0700

    toggling descriptor

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
-const list = document.querySelector('.masonry-layout');
+var list = document.querySelector('.masonry-layout');
 
 firebase.database().ref('/').once('value').then(function(snapshot) {
-  const fragment = document.createDocumentFragment();
+  var fragment = document.createDocumentFragment();
   var data = snapshot.val()
 
   Object.values(data).forEach(function(datum) {
@@ -9,20 +9,33 @@ firebase.database().ref('/').once('value').then(function(snapshot) {
     li.classList.add('masonry-layout__panel');
 
     var div = document.createElement('div');
-    div.classList.add('masonry-layout__panel-content')
+    div.classList.add('masonry-layout__panel-content');
+    div.classList.add('margin');
     var mySVG64 = window.btoa(datum.inline);
-    div.innerHTML = `
-      <a href="${datum.liveHref}" target="_blank" alt="${datum.title}">
-        <img class="lazyload"
+    var innerHTML = `<img class="lazyload pointer"
           alt="${datum.title}"
           src="data:image/svg+xml;base64,${mySVG64}"
           data-src="${datum.imgSrc}">
-      </a>`
+        <div class="hidden description">
+          <h3>${datum.title}</h3>
+          <div>${datum.desc}</div>`;
 
+    if (datum.githubHref) innerHTML += `<a href="${datum.githubHref}" target="_blank" alt="link to github code">github</a>`;
+    if (datum.liveHref) innerHTML += `<a href="${datum.liveHref}" target="_blank" alt="link to site">site</a>`;
+    innerHTML += `</div>`;
+
+    div.innerHTML = innerHTML;
     li.appendChild(div);
+    div.addEventListener('click', onClick)
     fragment.appendChild(li);
   })
 
   list.appendChild(fragment);
   lazyload();
 });
+
+function onClick(e) {
+  e.preventDefault();
+  var descriptorBlock = this.querySelector('.description');
+  descriptorBlock.classList.toggle('hidden');
+}
